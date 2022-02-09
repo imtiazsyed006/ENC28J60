e4b0aaea46d0f556c8abb3a21edc7f20ba03e0d9
imtiazsyed006 <imtiazsyed006@gmail.com>
2022-02-09 09:17:28 +0500

    main.c

---
+++ ENC28J60/main.c
@@ -0,0 +1,31 @@
+//#############################################################################
+//
+// FILE:  main.c
+//
+// TITLE:  ENC28J60 Ethernet Driver
+//
+// Empty SysCfg & Driverlib Example
+//
+// Author: Syed Imtiaz Ali Shah
+//
+//#############################################################################
+
+
+//
+// Included Files
+//
+#include "driverlib.h"
+#include "device.h"
+#include "board.h"
+
+//
+// Main
+//
+void main(void)
+{
+	Board_init();
+}
+
+//
+// End of File
+//
